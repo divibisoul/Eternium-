1a81993f6e449a85f5c485215759e33d3bd8c62a
divibisoul <divibi2011@gmail.com>
2026-08-27 01:57:50 -0300

    feat(n02): register N02 with N01 during boot

--- index.tsx
+++ index.tsx
@@ -1,17 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-3
+import { registerWithN01 } from './api/soul-mesh/peer-client';
+const bootN02Mesh=async()=>{try{await registerWithN01();}catch(error){console.warn('[N02 MESH] N01 indisponível ou registro adiado:',error);}};
+void bootN02Mesh();
+const rootElement=document.getElementById('root');
+if(!rootElement)throw new Error('Could not find root element to mount to');
+ReactDOM.createRoot(rootElement).render(<React.StrictMode><App/></React.StrictMode>);
